5722eda666ba199931522c21c467910aefb6f325
Phanindra731 <gudapatiphani31@gmail.com>
2023-05-22 16:18:01 +0530

    welcomepopups and homepage

--- environment.py
+++ environment.py
@@ -5,7 +5,7 @@ from base.BasePage import BasePage
 from base.DriverClass import Driver
 import utilities.CustomLogger as cl
 import configurationfiles.DeviceConfig as dc
-
+from behave.contrib.scenario_autoretry import patch_scenario_with_autoretry
 log = cl.customLogger()
 
 
@@ -16,9 +16,12 @@ def before_scenario(context, scenario):
     context.driver = context.driver1.getDriverMethod(context)
     context.bp = BasePage(context.driver)
     #context.bp.launchWebPage(dc.url, dc.title)
+    # for scenario in feature.scenarios:
+    #     if "smoke_test" in scenario.effective_tags:
+    #         patch_scenario_with_autoretry(scenario, max_attempts=3)
 
 
 def after_scenario(context, scenario):
-    time.sleep(5)
-    #context.driver.quit()
+    #time.sleep(1)
+    context.driver.quit()
     #log.info("Automation Ended")
